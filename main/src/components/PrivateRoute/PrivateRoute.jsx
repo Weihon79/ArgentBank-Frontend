4c6cdd41fdbb4e96846497c16367d8e737803a22
Weihon79 <ervan.chin@gmail.com>
2025-04-21 10:55:09 +0200

    ajout d'un selecteur manquant

--- main/src/components/PrivateRoute/PrivateRoute.jsx
+++ main/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "../../features/authSlice";
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); 
+  const isAuthenticated = useSelector(selectIsAuthenticated); 
 
   return isAuthenticated ? children : <Navigate to="/sign-in" />; 
 };
